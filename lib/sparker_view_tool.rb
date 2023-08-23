606c09db81a1cd500499b545f0803436d04d8619
Sean Parker <seanrichardparker@gmail.com>
2023-08-23 15:14:02 -0400

    View Tool gem init commit

---
+++ lib/sparker_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "sparker_view_tool/version"
+require_relative "sparker_view_tool/renderer"
+
+module SparkerViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
